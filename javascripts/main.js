4a50c3172f688a4bb911ccf1392e83f473a2aba3
mniedre <webcane@ya.ru>
2014-03-09 22:16:01 +0400

    add open site fix filter links

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,67 +1,74 @@
 /* my custom scripts */
 jQuery(document).ready(function() {
   
-  // smooth scrolling for internal anchor links
-  $('a[href*=#]:not([href=#])').click(function(event){
-      var target = $(this.hash);		
-      if (target.length) {
-        //event.preventDefault();
-  		  $('html,body').animate({scrollTop: target.offset().top}, 900);
-                return false; 
-      }
-  });
+	  // smooth scrolling for internal anchor links
+	  $('a[href*=#about]').click(function(event){
+		  $('.site-wrapper').css("height", "auto");
+	  });
+	  
+	  // smooth scrolling for internal anchor links
+	  $('a[href*=#]:not([href=#])').click(function(event){
+	      var target = $(this.hash);		
+	      if (target.length) {
+	        //event.preventDefault();
+	  		  $('html,body').animate({scrollTop: target.offset().top}, 900);
+	                return false; 
+	      }
+	  });
   
-  // animate about
-  $('#about').waypoint(function() {
-        $('#about h2').addClass('d7 animated fadeInDown').addClass('in');
-        $('.author_foto').addClass('d6 animated rotateInDownLeft').addClass('in');
-		$('.author_words').addClass('d8 animated lightSpeedIn').addClass('in');
-     }, { 
-        triggerOnce: true, offset: 395
-  });
-
-  // animate portfolio
-  $('#portfolio').waypoint(function() {
-        $('#portfolio h2').addClass('d3 animated bounceIn').addClass('in');
-		$('#portfolio .filter_controls').addClass('d4 animated bounceInRight').addClass('in');
-		$('#portfolio .portfolio_description').addClass('d2 animated bounceInLeft').addClass('in');
-		$('#portfolio #portfolio_grid').addClass('d7 animated bounceInRight').addClass('in');
-    }, { 
-        triggerOnce: true, offset: 395
-	});
+	  // hide elements for animation
+	  initOpacity();
+  
+	  // animate about
+	  $('#about').waypoint(function() {
+	        $('#about h2').addClass('d7 animated fadeInDown').addClass('in');
+	        $('.author_foto').addClass('d6 animated rotateInDownLeft').addClass('in');
+			$('.author_words').addClass('d8 animated lightSpeedIn').addClass('in');
+	     }, { 
+	        triggerOnce: true, offset: 395
+	  });
 	
-  $('#portfolio_grid .mix').addClass('mixitup_fade');  
-  // INSTANTIATE MIXITUP		
-  $('#portfolio_grid').mixitup();
-
-	
-  /* show grid in 2 or 3 columns */
-  $('[data-toggle=cols]').click(function() {
-	  mix = $('.mix');
-	  mix.toggleClass('col-12');
-	  mix.toggleClass('col-md-6');
-	  mix.toggleClass('col-lg-6');
-	  mix.toggleClass('col-6');    
-	  mix.toggleClass('col-md-4');
-	  mix.toggleClass('col-lg-4');  
-  });
+	  // animate portfolio
+	  $('#portfolio').waypoint(function() {
+	        $('#portfolio h2').addClass('d3 animated bounceIn').addClass('in');
+			$('#portfolio .filter_controls').addClass('d4 animated bounceInRight').addClass('in');
+			$('#portfolio .portfolio_description').addClass('d2 animated bounceInLeft').addClass('in');
+			$('#portfolio #portfolio_grid').addClass('d7 animated bounceInRight').addClass('in');
+	    }, { 
+	        triggerOnce: true, offset: 395
+		});
 		
-		/* show grid images responsive or not */
-  $('[data-toggle=imgs-resp]').click(function() {
-	  $('.mix img').toggleClass('img-responsive');  
-  });
-
-  
-	//Home Height and W
- // blockFullScreen('#home');
- // blockFullScreen('#about');
- // blockFullScreen('#portfolio');  
+	  $('#portfolio_grid .mix').addClass('mixitup_fade');  
+	  // INSTANTIATE MIXITUP		
+	  $('#portfolio_grid').mixitup();
 	
-  //Welcome Block Vertical Align
-  welcomeVerticalCenter();	
+		
+	  /* show grid in 2 or 3 columns */
+	  $('[data-toggle=cols]').click(function() {
+		  mix = $('.mix');
+		  mix.toggleClass('col-12');
+		  mix.toggleClass('col-md-6');
+		  mix.toggleClass('col-lg-6');
+		  mix.toggleClass('col-6');    
+		  mix.toggleClass('col-md-4');
+		  mix.toggleClass('col-lg-4');  
+	  });
+			
+	  /* show grid images responsive or not */
+	  $('[data-toggle=imgs-resp]').click(function() {
+		  $('.mix img').toggleClass('img-responsive');  
+	  });
+	
+	  
+		//Home Height and W
+	 // blockFullScreen('#home');
+	 // blockFullScreen('#about');
+	 // blockFullScreen('#portfolio');  
+		
+	  //Welcome Block Vertical Align
+	  welcomeVerticalCenter();	
   
-  // hide elements for animation
-  initOpacity();
+
   
   
 });
@@ -80,7 +87,7 @@ jQuery(window).resize(function(){
 //Home Height & Width
 function blockFullScreen(elem){
 	wh = $(window).height();
-  ww = $(window).width();
+	ww = $(window).width();
 	$(elem).css({min_height:wh, width:ww});
 }
 
